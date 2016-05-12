ac2cea0dd0ec30f26a659e8efd5c36c556d646a1
Hedvre <valentin.lepetit@free.fr>
2016-05-12 13:34:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ void accentueContraste(int image[N][N]){
 
     //remplacement
     for (int x=0 ; x<N ; x++) {
-        for (int y=0 ; y<N ; y++)
+        for (int y=0 ; y<N ; y++){
             // ne pas oublier de multiplier par 10
             //cond 1 < a la moyenne && cond2 >= 1 pour ne pas descendre en dessous de 0
             if(10*image[x][y] < moyenne && 10*image[x][y] >= 10){
